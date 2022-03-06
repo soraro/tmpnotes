94bde141d12004367cc5c8a912830d507abf34f2
kyle <sorensenkw@gmail.com>
2022-03-06 08:28:58 -0700

    initial health check

---
+++ internal/health/health.go
@@ -0,0 +1,32 @@
+package health
+
+import (
+	"encoding/json"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+
+	log.Info(r.RequestURI)
+
+	if r.Method == "GET" {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "applications/json")
+		resp := make(map[string]string)
+		resp["message"] = "Status OK"
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Error("healthcheck response format failed")
+			http.Error(w, "healthcheck failed", 500)
+		}
+		w.Write(jsonResp)
+		return
+	} else {
+		log.Errorf("healthcheck failed: %s %s", r.RequestURI, r.Method)
+		http.Error(w, "404 Not Found", 404)
+		return
+	}
+
+}
