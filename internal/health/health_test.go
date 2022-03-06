77a9f0b99db6c5f5e0243e308100369c5cf176fd
kyle <sorensenkw@gmail.com>
2022-03-06 15:50:25 -0700

    tests added

--- internal/health/health_test.go
+++ internal/health/health_test.go
@@ -2,23 +2,55 @@ package health
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestHealthCheck(t *testing.T) {
 	type args struct {
-		w http.ResponseWriter
-		r *http.Request
+		w              http.ResponseWriter
+		r              *http.Request
+		expectedStatus int
 	}
+
 	tests := []struct {
 		name string
 		args args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "GET method",
+			args: args{
+				w:              httptest.NewRecorder(),
+				r:              httptest.NewRequest(http.MethodGet, "/healthz", nil),
+				expectedStatus: 200,
+			},
+		},
+		{
+			name: "POST method",
+			args: args{
+				w:              httptest.NewRecorder(),
+				r:              httptest.NewRequest(http.MethodPost, "/healthz", nil),
+				expectedStatus: 405,
+			},
+		},
+		{
+			name: "PUT method",
+			args: args{
+				w:              httptest.NewRecorder(),
+				r:              httptest.NewRequest(http.MethodPut, "/healthz", nil),
+				expectedStatus: 405,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			HealthCheck(tt.args.w, tt.args.r)
+			w := httptest.NewRecorder()
+			HealthCheck(w, tt.args.r)
+			res := w.Result()
+
+			if res.StatusCode != tt.args.expectedStatus {
+				t.Errorf("Unexpected Status Code")
+			}
 		})
 	}
 }
