87433f7de4bf6cba4a57973883d03b47365c7dd4
kyle <sorensenkw@gmail.com>
2022-03-06 13:39:58 -0700

    update health status handling

--- internal/health/health.go
+++ internal/health/health.go
@@ -15,7 +15,7 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		resp := make(map[string]string)
-		resp["message"] = "Status OK"
+		resp["message"] = "200 Status OK"
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
 			log.Errorf("%s healthcheck response format failed", r.RequestURI)
@@ -25,7 +25,16 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 		return
 	} else {
 		log.Errorf("%s healthcheck failed: %s", r.RequestURI, r.Method)
-		http.Error(w, "404 Not Found", 404)
+		w.Header().Set("Allow", "GET")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		resp := make(map[string]string)
+		resp["message"] = "405 Not Allowed"
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Errorf("%s healthcheck response format failed", r.RequestURI)
+			http.Error(w, "healthcheck failed", 500)
+		}
+		w.Write(jsonResp)
 		return
 	}
 
