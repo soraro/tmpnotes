b7659696248f6f8462f166c748a7ee39336c0f77
Aaron <aaron710@gmail.com>
2022-04-16 20:50:28 -0600

    Adding test

---
+++ internal/config/config_test.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func Test_GetConfig(t *testing.T) {
+	tests := []struct {
+		name string
+		args map[string]string
+		want specification
+	}{
+		{
+			name: "Test 1",
+			args: map[string]string{"PORT": "5000", "REDIS_URL": "redis://localhost:1234"},
+			want: specification{Port: 5000, RedisUrl: "redis://localhost:1234"},
+		},
+		{
+			name: "Test 2",
+			args: map[string]string{"TMPNOTES_PORT": "6000", "TMPNOTES_REDIS_URL": "rediss://someserver:1234"},
+			want: specification{Port: 6000, RedisUrl: "rediss://someserver:1234"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for env, val := range tt.args {
+				os.Setenv(env, val)
+				defer os.Unsetenv(env)
+			}
+			GetConfig()
+			if !reflect.DeepEqual(Config, tt.want) {
+				t.Errorf("%s: GetConfig() = %v, want %v", tt.name, Config, tt.want)
+			}
+		})
+	}
+}
