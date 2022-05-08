b3d97a47b77c0e1150feca05fa0e33dfb4c0e7bc
Aaron <aaron710@gmail.com>
2022-05-07 20:48:12 -0600

    adding test for GetTemplates()

--- internal/config/template.go
+++ internal/config/template.go
@@ -7,10 +7,11 @@ import (
 var (
 	Tmpl *template.Template
 	err  error
+	path = "templates/*"
 )
 
 func GetTemplates() error {
-	Tmpl, err = template.ParseGlob("templates/*")
+	Tmpl, err = template.ParseGlob(path)
 	if err != nil {
 		return err
 	}
